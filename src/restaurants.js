dcc773913bdc8b8c9e424458640cf5c5a2d30c7b
Tuure Savuoja <tuure@tuuresavuoja.net>
2016-09-28 23:18:17 +0300

    update data

--- src/restaurants.js
+++ src/restaurants.js
@@ -1,22 +1,27 @@
 export default [
   ['Pajazzo', 'http://www.royalravintolat.com/casinohelsinki/piazza-lounas'],
   ['Factory Aleksi', 'http://www.ravintolafactory.com/aleksi/lounas/'],
-  ['Mount Everest Vilhola', 'http://www.everestyeti.fi/fi/vilhola/lounas.php'],
-  ['Base Camp', 'http://www.basecampnepal.fi/lounaslista'],
+  ['Mount Everest Vilhola', 'http://www.everestyeti.fi/vilhola/fi/index.php'],
+  ['Base Camp', 'http://www.basecampnepal.fi/helsinki/fi/index.php'],
   ['Maya', 'http://www.maya.fi/lounaat.php'],
-  ['Kitzens Radisson Blu Plaza', 'http://www.raflaamo.fi/helsinki/' +
-    'kitzens-helsinki-radisson-blu-plaza' +
-    '#navigation_raflaamolunchmenuportlet_war_raflaamorestaurantportlet'],
+  ['Frans & Amélie', 'https://www.raflaamo.fi/fi/helsinki/frans-amelie-helsinki#/#browsing'],
+  [
+    'Kitzens Radisson Blu Plaza',
+    'http://www.raflaamo.fi/helsinki/kitzens-helsinki-radisson-blu-plaza#'
+      + 'navigation_raflaamolunchmenuportlet_war_raflaamorestaurantportlet',
+  ],
   ['Tablo Ateneum', 'https://www.facebook.com/TabloAteneum'],
   ['Il Siciliano', 'http://ilsiciliano2.fi/lounas/'],
-  ['La Torrefazione', 'http://www.latorre.fi/'],
-  ['Fratello Torrefazione', 'http://fratello.fi/'],
+  ['La Torrefazione', 'http://www.latorre.fi/toimipiste/aleksanterinkatu'],
+  ['Fratello Torrefazione', 'http://www.latorre.fi/toimipiste/fratello'],
   ['Woolshed', 'http://www.woolshed.eu/#!lounas-menu/ceik'],
-  ['Armas', 'http://armashelsinki.fi/'],
   ['Belge', 'http://www.belge.fi/belge-lounas.html'],
   ['Morrisonʼs', 'http://www.lounaat.fi/#morrisons'],
-  ['Memphis Hesari', 'http://www.raflaamo.fi/fi/helsinki/memphis-hesari' +
-    '#navigation_raflaamolunchmenuportlet_war_raflaamorestaurantportlet'],
+  [
+    'Memphis Hesari',
+    'http://www.raflaamo.fi/fi/helsinki/memphis-hesari#'
+      + 'navigation_raflaamolunchmenuportlet_war_raflaamorestaurantportlet',
+  ],
   ['Leonardo', 'http://www.leonardobarristorante.fi/lounas'],
   ['Sports Academy', 'http://www.sportsacademy.fi/lounas.html'],
   ['Shanghai Cowboy', 'https://www.facebook.com/ShanghaiCowboyHKI/'],
@@ -24,5 +29,6 @@ export default [
 ]
 
 // RIP
-// ['Café Balzac', 'http://www.cafebalzac.fi/menu.php'],
-// ['Meze Point', 'http://www.meze.fi/lounas'],
+//   ['Café Balzac', 'http://www.cafebalzac.fi/menu.php']
+//   ['Meze Point', 'http://www.meze.fi/lounas']
+//   ['Armas', 'http://armashelsinki.fi/']
